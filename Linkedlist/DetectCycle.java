c223fb6d5990c338ad0e935e9158c2f1eb53fc67
Riya Srivastava <156165467+riya810sri@users.noreply.github.com>
2025-10-04 15:13:51 +0530

    Add files via upload

---
+++ Linkedlist/DetectCycle.java
@@ -0,0 +1,39 @@
+package linklist;
+public class DetectCycle {
+
+public static class Node{
+   int data;
+   Node next;
+   public Node(int data){
+      this.data=data;
+      this.next=null;
+   }
+}
+
+public static  Node head;
+Node tail;
+public static  Boolean isCycle(){
+   
+   Node slow=head;
+   Node fast=head;
+   while(fast!=null && fast.next!=null){
+   slow=slow.next;
+   fast=fast.next.next;
+   if(slow==fast){
+         return true;
+      }
+}
+return false;
+}
+
+public static void main (String args[]){
+   head=new Node(1);
+   head.next=new Node(2);
+   head.next.next=new Node(3);
+   head.next.next.next=head;
+
+ System.out.print(isCycle());
+  
+}
+
+}
